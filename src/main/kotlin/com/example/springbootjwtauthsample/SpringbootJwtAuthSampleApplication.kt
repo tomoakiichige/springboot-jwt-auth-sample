2526685193c776bad605497c869f44a965969cec
Tomoaki Ichige <ichigetomoaki1207@gmail.com>
2024-06-25 04:04:31 +0900

    init

---
+++ src/main/kotlin/com/example/springbootjwtauthsample/SpringbootJwtAuthSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springbootjwtauthsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootJwtAuthSampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringbootJwtAuthSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springbootjwtauthsample/SpringbootJwtAuthSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springbootjwtauthsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootJwtAuthSampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
